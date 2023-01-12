72c0271f2f94dda4ca27832783b38dc42253dcd4
ioriqC <ioriqC@gmail.com>
2023-01-12 20:12:25 +0200

    upload project

---
+++ src/components/Form.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useDispatch } from 'react-redux';
+import { v4 } from 'uuid';
+import { addTodo } from '../features/todo/todoSlice';
+
+const Form = () => {
+  const [todoValue, setTodoValue] = React.useState('')
+  const dispatch = useDispatch()
+  
+  const addTodoHandler = () => {
+    const todo = {
+      id: v4(),
+      text: todoValue,
+      completed: false,
+    }
+    dispatch(addTodo(todo))
+    setTodoValue('')
+  }
+
+    return (
+        <form className='w-full flex' onSubmit={(e) => e.preventDefault()}>
+            <input
+                type='text'
+                value={todoValue}
+                onChange={(e) => setTodoValue(e.target.value)}
+                placeholder='Type something...'
+                className='w-full p-1 focus:outline-none focus:border-lime-500 focus: border-2 placeholder:text-sm'
+            />
+            <button
+                onClick={() => addTodoHandler()}
+                type='submit'
+                className='shrink-0 bg-lime-300  hover:bg-lime-400 transition-all px-3 text-sm'
+            >
+                Submit
+            </button>
+        </form>
+    )
+}
+
+export default Form
---
+++ src/features/todo/todoSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  todos: [],
+}
+
+export const todoSlice = createSlice({
+  name: 'todo',
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      state.todos.push(action.payload)
+    },
+    toggleCompletedTodo: (state, action) => {
+      const toggleTodo = state.todos.find((todo) => todo.id === action.payload)
+      toggleTodo.completed = !toggleTodo.completed
+    },
+    deleteTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+    }
+   
+  },
+})
+
+export const { addTodo, toggleCompletedTodo, deleteTodo } = todoSlice.actions
+
+export default todoSlice.reducer
